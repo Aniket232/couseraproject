7e137bbf5c298398156902b3ba277699df2f1e37
Aniket Manna <senanik45@gmail.com>
2020-09-27 19:25:34 +0530

    first  component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AniketComponent } from './aniket/aniket.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AniketComponent
   ],
   imports: [
     BrowserModule
